73f295e822893cfd69c69b7db969d991f57b5a73
Victor Delépine <vctr.delepine@gmail.com>
2014-05-05 18:53:10 +0200

    Ajout des css bootstrap et flat-ui

--- tweets/tweets/settings.py
+++ tweets/tweets/settings.py
@@ -10,6 +10,8 @@ https://docs.djangoproject.com/en/1.6/ref/settings/
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
+
+PROJECT_HOME = os.path.dirname(os.path.dirname(__file__))
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
 
@@ -79,11 +81,32 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 
+# Absolute path to the directory static files should be collected to.
+# Don't put anything in this directory yourself; store your static files
+# in apps' "static/" subdirectories and in STATICFILES_DIRS.
+# Example: "/var/www/example.com/static/"
+STATIC_ROOT = os.path.join(PROJECT_HOME, 'collected_static/')
+# URL prefix for static files.
+# Example: "http://example.com/static/", "http://static.example.com/"
 STATIC_URL = '/static/'
 
-import os
-PROJECT_HOME = os.path.dirname(os.path.dirname(__file__))
+# Additional locations of static files
+STATICFILES_DIRS = (
+    # Put strings here, like "/home/html/static" or "C:/www/django/static".
+    # Always use forward slashes, even on Windows.
+    # Don't forget to use absolute paths, not relative paths.
+    os.path.join(BASE_DIR, "static"),
+)
+
+print STATICFILES_DIRS
+
+# List of finder classes that know how to find static files in
+# various locations.
+STATICFILES_FINDERS = (
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+)
 
 TEMPLATE_DIRS = (
-    PROJECT_HOME + "/templates"    
+    PROJECT_HOME + "/templates"
 )
--- tweets/tweets/urls.py
+++ tweets/tweets/urls.py
@@ -1,12 +1,14 @@
 from django.conf.urls import patterns, include, url
-
-from django.contrib import admin
-admin.autodiscover()
+import settings
 
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'tweets.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
     url(r'^$', 'home.views.welcome'),
-    url(r'^admin/', include(admin.site.urls)),
 )
+
+if not settings.DEBUG:
+    from django.conf.urls.static import static
+
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
